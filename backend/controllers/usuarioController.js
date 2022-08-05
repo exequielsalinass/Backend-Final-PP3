3b6c43ec97ab815a458741a39233f27edd0c45e1
Exequiel <e.x.e_salinas3@outlook.com>
2022-08-05 12:54:13 -0300

    Probando valicaciones de passwords

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -6,9 +6,31 @@ import generarJWT from "../helpers/generarJWT.js";
 
 const registrar = async (req, res) => {                         //* req: datos que envio || res: datos que obtengo
   // Evitar registros duplicados
-  const { email } = req.body;                               
+  const { email, password } = req.body;                            
   const existeUsuario = await Usuario.findOne({ email });
 
+  // Validaciones para la password
+  if(!password.match(/[A-z]/)) {
+    const error = new Error("La contraseña debe contar con una letra");
+    return res.status(400).json({ msg: error.message });
+  }
+
+  if(!password.match(/[A-Z]/)) {
+    const error = new Error("La contraseña debe contar con una letra mayúscula");
+    return res.status(400).json({ msg: error.message });
+  }
+
+  if(!password.match(/\d/)) {
+    const error = new Error("La contraseña debe contar con un número");
+    return res.status(400).json({ msg: error.message });
+  }
+
+  if(password.length < 8) {
+    const error = new Error("La contraseña debe ser igual o mayor a 8 caracteres");
+    return res.status(400).json({ msg: error.message });
+  }
+
+  // Evitar registros duplicados
   if (existeUsuario) {
     const error = new Error("Usuario ya registrado");
     return res.status(400).json({ msg: error.message });
@@ -80,4 +102,28 @@ const confirmar = async (req, res) => {
   }
 };
 
-export { registrar, autenticar, confirmar }
+const olvidePassword = async (req, res) => {
+  const { email } = req.body;
+  const usuario = await Usuario.findOne({ email });
+  if (!usuario) {
+    const error = new Error("El Usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+  try {
+    usuario.token = generarId();
+    await usuario.save();
+
+    //Enviar el email
+    emailOlvidePassword({
+      email: usuario.email,
+      nombre: usuario.nombre,
+      token: usuario.token
+    })
+
+    res.json({ msg: "Hemos enviado un email con las instrucciones" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { registrar, autenticar, confirmar, olvidePassword }
--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -4,7 +4,8 @@ const router = express.Router();
 import {
   registrar,
   autenticar,
-  confirmar
+  confirmar,
+  olvidePassword
 } from "../controllers/usuarioController.js";
 
 
@@ -12,8 +13,8 @@ import {
 router.post("/", registrar); //Crea un nuevo usuario
 router.post("/login", autenticar); //Authentication
 router.get("/confirmar/:token", confirmar); //Token
-/* router.post("/olvide-password", olvidePassword); // Enviar instrucciones para cambiar el password
-router.get("/olvide-password/:token", comprobarToken); // Validar el nuevo token
+router.post("/olvide-password", olvidePassword); // Enviar instrucciones para cambiar el password
+/* router.get("/olvide-password/:token", comprobarToken); // Validar el nuevo token
 router.post("/olvide-password/:token", nuevoPassword); // Obtener el nuevo password
 // router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword) //* Otra alternativa para las lineas 11 y 12
 router.get("/perfil", checkAuth, perfil); */
